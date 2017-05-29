1d2811d9ff34756146ece183b2d799c6ba10a279
Christer Kaitila <github@mcfunkypants.com>
2017-05-29 11:10:27 -0700

    canvas automatically resizes responsively

--- js/Main.js
+++ js/Main.js
@@ -2,31 +2,52 @@
 var canvas;
 var canvasContext;
 
+const FULL_SIZE_CANVAS = true; // responsive resizing?
+
 var gameManager = new GameManager();
 var inputManager = new inputManager();
 
 
 
 window.onload = function() {
-    canvas = document.getElementById('gameCanvas');
-  
-if (USE_WEBGL_IF_SUPPORTED)	init_webGL(); // now!
 
-  canvasContext = canvas.getContext('2d');
-  	if (!canvasContext) {
-		console.log("ERROR: no canvasContext!");
-		// this can happen if webGL grabs it instead
-	}
-  
+    console.log("Initializing game engine...");
+    
+    canvas = document.getElementById('gameCanvas');
+
+    if (FULL_SIZE_CANVAS) {
+        window.addEventListener("resize", onResize);
+        onResize();
+    }
+
+    if (USE_WEBGL_IF_SUPPORTED)	{
+        init_webGL();
+    }
+
+    canvasContext = canvas.getContext('2d');
+
+    if (!canvasContext) {
+        console.log("canvasContext is null. We must be in webGL mode.");
+    }
+
     loadImages();
 }
 
+function onResize() { // full screen
+    if (!canvas) return;
+    console.log("resizing canvas to "+window.innerWidth+","+window.innerHeight);
+    canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+
 function loadingDoneSoStartGame() {
     var framesPerSecond = 60;
+    
+    // FIXME: for performance (and battery saving) we should use requestAnimationFrame(gameManager.update);
     setInterval(function() {
         gameManager.update();
         }, 1000/framesPerSecond);
-
 
     gameManager.initialize();
 }
